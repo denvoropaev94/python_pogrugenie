47fa02dae29f7692d449b84279581833630013a6
«Denis» <denvoropaev7@gmail.com>
2023-06-23 15:37:30 +0300

    задачи с 6-го семинара + решение 1 дз про модуль дата

---
+++ home_work_6/__init__.py
@@ -0,0 +1,3 @@
+from .seminar6 import zagad, guess, puzzle, func_info, my_date_info
+
+__all__ = ['guess', 'zagad', 'puzzle', 'func_info', 'my_date_info']
---
+++ home_work_6/z1_homework_6.py
@@ -0,0 +1,4 @@
+from seminar6 import my_date_info
+import sys
+
+print(my_date_info(sys.argv[1]))
