7cbc12ebf651d3877523d3a4722f53fed0382fb7
«Denis» <denvoropaev7@gmail.com>
2023-06-30 17:40:11 +0300

    функция, которая получает на вход директорию и рекурсивно обходит её и все вложенные директории, Для каждого объекта указано файл это или директория. Для файлов - размер в байтах, а для директорий размер файлов в ней с учётом всех вложенных файлов и директорий. Добавлено записи в файл

--- home_work_8/hw.py
+++ home_work_8/hw.py
@@ -1,5 +1,7 @@
 import json
 import os
+import pickle
+import csv
 
 path = os.getcwd()
 
@@ -11,18 +13,24 @@ def recursive_traversal(path, level=1):
         if os.path.isfile(path + '/' + i):
             size = os.path.getsize(path + '/' + i)
             print(i, 'This is file', 'Размер:', size)
-            # my_dict[i] = size
+            my_dict[i] = size
         elif os.path.isdir(path + '/' + i):
             size = os.path.getsize(path + '/' + i)
             print(i, 'This is dir', 'Размер:', size)
-            # my_dict[i] = size
+            my_dict[i] = size
             print('Спускаемся', path + '/' + i)
             recursive_traversal(path + '/' + i, level + 1)
             print('Возвращаемся в ', path)
     if my_dict:
         with open('my_file.json', 'a', encoding='utf-8') as f:
             json.dump(my_dict, f)
-    # print(my_dict)
+        with open('my.pickle', 'ab') as f1:
+            pickle.dump(my_dict, f1)
+        with open('my_file.csv', 'a') as f2:
+            csv_file = csv.DictWriter(f2, fieldnames=[value for value in my_dict],
+                                      dialect='excel', quoting=csv.QUOTE_ALL)
+            csv_file.writeheader()
+# print(my_dict)
 
 
 recursive_traversal(path)
