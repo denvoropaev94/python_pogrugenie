5791943dd44f65c5df1da4c0e6b9b17c95e4cd61
«Denis» <denvoropaev7@gmail.com>
2023-07-11 15:21:47 +0300

    Класс моя строка на основе str

---
+++ home_work_11/seminar_11/task_01.py
@@ -0,0 +1,18 @@
+# Задание No1
+# Создайте класс Моя Строка, где:
+# будут доступны все возможности str
+# дополнительно хранятся имя автора строки и время создания (time.time)
+import datetime
+import time
+
+
+class MyStr(str):
+
+    def __new__(cls, value, name):
+        instance = super().__new__(cls, value)
+        instance.name = name
+        instance.date = datetime.datetime.now()
+        return instance
+
+first = MyStr('Real Madrid', 'Voropaev')
+print(f'Название: {first}, автор: {first.name}, time: {first.date}')
